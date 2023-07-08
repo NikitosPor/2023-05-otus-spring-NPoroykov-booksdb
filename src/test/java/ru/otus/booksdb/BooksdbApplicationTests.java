002b0c1fc1ca539e69c7813edafa04e633347997
Nikita P <nporoykov@gmail.com>
2023-07-08 08:09:38 +0300

    init commit

---
+++ src/test/java/ru/otus/booksdb/BooksdbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.booksdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
